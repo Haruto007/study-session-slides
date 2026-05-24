8e3ef64bbb11ff7a62dbf7f21e3d908670bb95d5
鈴木温人 <suzukiharuto@Hs-Macbook.local>
2026-05-24 20:54:20 +0900

    Add AI bird study session slide SPA

--- src/コード.js
+++ src/コード.js
@@ -1,3 +1,6 @@
-function myFunction() {
-  
+function doGet() {
+  return HtmlService
+    .createHtmlOutputFromFile('index')
+    .setTitle('AIバード勉強会 | 継続支援につながる実践ノウハウ')
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
 }
